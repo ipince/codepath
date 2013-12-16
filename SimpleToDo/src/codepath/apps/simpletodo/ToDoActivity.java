dd02f5881df70da5fbc966b41678751eb5559484
Rodrigo Ipince <rodrigo.ipince@gmail.com>
2013-12-16 00:49:27 -0800

    persists items to file

--- SimpleToDo/src/codepath/apps/simpletodo/ToDoActivity.java
+++ SimpleToDo/src/codepath/apps/simpletodo/ToDoActivity.java
@@ -1,21 +1,26 @@
 package codepath.apps.simpletodo;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.commons.io.FileUtils;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
 public class ToDoActivity extends Activity {
 
-    private final List<String> items = new ArrayList<String>();
-    private ListAdapter itemsAdapter;
+    private List<String> items = new ArrayList<String>();
+    private ArrayAdapter<String> itemsAdapter;
 
     // UI elements
     private EditText etAddItem;
@@ -29,10 +34,13 @@ public class ToDoActivity extends Activity {
         etAddItem = (EditText) findViewById(R.id.etAddItem);
         lvItems = (ListView) findViewById(R.id.lvItems);
 
+        loadItems();
         itemsAdapter = new ArrayAdapter<String>(
                 getBaseContext(), android.R.layout.simple_list_item_1, items);
 
         lvItems.setAdapter(itemsAdapter);
+
+        setupListViewListener();
     }
 
     @Override
@@ -43,7 +51,41 @@ public class ToDoActivity extends Activity {
     }
 
     public void addItem(View v) {
-        items.add(etAddItem.getText().toString());
+        itemsAdapter.add(etAddItem.getText().toString());
         etAddItem.setText("");
+        saveItems();
+    }
+
+    private void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(new OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapter, View view, int pos, long id) {
+                items.remove(pos);
+                itemsAdapter.notifyDataSetChanged();
+                saveItems();
+                return true;
+            }
+        });
+    }
+
+    private void loadItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+        } catch (IOException e) {
+            // no file (well, not really, but OK for now)
+            items = new ArrayList<String>();
+        }
+    }
+
+    private void saveItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
