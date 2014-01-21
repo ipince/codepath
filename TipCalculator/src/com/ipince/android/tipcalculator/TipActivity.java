ae0a1502116ff053512093086b794478832f10b5
Rodrigo Ipince <rodrigo.ipince@gmail.com>
2014-01-20 22:56:11 -0800

    added rounded tip functionality

--- TipCalculator/src/com/ipince/android/tipcalculator/TipActivity.java
+++ TipCalculator/src/com/ipince/android/tipcalculator/TipActivity.java
@@ -22,6 +22,13 @@ public class TipActivity extends Activity {
 
     private final int TOAST_TIME_SECS = 2;
 
+    /**
+     * Threshold used to decide whether to round tips up or down so that the total is a whole dollar
+     * amount. Setting this to 50 would correspond to plain old rounding. Usually set below 50 so
+     * that tips aren't impacted much.
+     */
+    private final int ROUNDING_THRESHOLD_CENTS = 25;
+
     private EditText etFullAmount;
     private Button bTipPctLeft;
     private Button bTipPctMiddle;
@@ -48,14 +55,6 @@ public class TipActivity extends Activity {
         bTipPctRight.setOnClickListener(getListener(0.2));
 
         // TODO: add on change value listeners
-        // TODO: add rounded tip calculation
-    }
-
-    private void setTipAmount(int amountCents, int tipCents) {
-        double total = (amountCents + tipCents)*1.0/100;
-        tvTipAmount.setText(getString(R.string.tip_message,
-                formatter.format(tipCents*1.0/100),
-                formatter.format(total)));
     }
 
     private OnClickListener getListener(final double pct) {
@@ -66,15 +65,37 @@ public class TipActivity extends Activity {
                 try {
                     Number num = formatter.parse(input);
                     int amountCents = (int) Math.round(num.doubleValue() * 100);
-                    int tipCents = (int) Math.round(amountCents * pct);
-                    setTipAmount(amountCents, tipCents);
+                    setTip(amountCents, pct, true);
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
                 } catch (ParseException e) {
                     Log.i(TAG, "Invalid currency: " + input, e);
                     Toast.makeText(TipActivity.this, getString(R.string.amount_error_msg), TOAST_TIME_SECS).show();
                 }
-                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(v.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
             }
         };
+    }
+
+    private void setTip(int amountCents, double targetPct, boolean round) {
+        // Calculate tip.
+        int tipCents = (int) Math.round(amountCents * targetPct);
+        if (round) {
+            // Round total up if above threshold, otherwise down.
+            int totalCents = amountCents + tipCents;
+            int rem = totalCents % 100;
+            if (rem > ROUNDING_THRESHOLD_CENTS) {
+                tipCents = tipCents + 100 - rem;
+            } else {
+                tipCents = tipCents - rem;
+            }
+        }
+
+        // Set UI.
+        double actualPct = (tipCents * 1.0 / amountCents) * 100.0;
+        double tip = tipCents / 100.0;
+        double total = (amountCents + tipCents) / 100.0;
+        tvTipAmount.setText(getString(R.string.tip_message,
+                formatter.format(tip), formatter.format(actualPct),
+                formatter.format(total)));
     }
 }
